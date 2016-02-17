30a886ebc324b74b00418dd08c0195e1d55d5eba
Adam Kinzer <adamkinzer915@gmail.com>
2016-02-17 11:55:43 -0700

    Added Timer to the project.

---
+++ src/math/model/RecursionTimer.java
@@ -0,0 +1,19 @@
+package math.model;
+
+public class RecursionTimer
+{
+	
+	private long executionTime;
+	
+	public CodeTimer()
+	{
+		this.executionTime = 0;
+	}
+	
+	public void startTime()
+	{
+		this.executionTime = System.nanoTime();
+	}
+	
+	public 
+}
